be2ac9af62f403db81c76010df066dab75ecd3d8
nikita <nikitamazurenko42@gmail.com>
2023-02-10 19:03:08 +0200

    implemented and added CsrfCookieFilter

--- src/main/java/com/mazurenko/springsecuritybasic/config/SecurityConfig.java
+++ src/main/java/com/mazurenko/springsecuritybasic/config/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.mazurenko.springsecuritybasic.config;
 
+import com.mazurenko.springsecuritybasic.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -19,28 +24,52 @@ public class SecurityConfig {
 
     // Adding custom security requirements
     @Bean
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .cors().configurationSource(request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("http://localhost:4200"));
-                config.setAllowedMethods(List.of("*"));
-                config.setAllowCredentials(true);
-                config.setAllowedHeaders(List.of("*"));
-                config.setMaxAge(3600L);
-                return config;
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+
+        /**
+         *  From Spring Security 6, below actions will not happen by default,
+         *  1) The Authentication details will not be saved automatically into SecurityContextHolder. To change this behaviour either we need to save
+         *      these details explicitly into SecurityContextHolder or we can configure securityContext().requireExplicitSave(false) like shown below.
+         *  2) The Session & JSessionID will not be created by default. Inorder to create a session after intial login, we need to configure
+         *      sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS)) like shown below.
+         */
+        http.securityContext().requireExplicitSave(false)
+            .and()
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+            .cors().configurationSource(new CorsConfigurationSource() {
+                @Override
+                public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                    config.setAllowedMethods(Collections.singletonList("*"));
+                    config.setAllowCredentials(true);
+                    config.setAllowedHeaders(Collections.singletonList("*"));
+                    config.setMaxAge(3600L);
+                    return config;
+                }
+                /**
+                 *  In Spring Security 5, the default behavior is that the CsrfToken will be loaded on every request. Where as with
+                 *  Spring Security 6, the default is that the lookup of the CsrfToken will be deferred until it is needed. The developer
+                 *  has to write logic to read the CSRF token and send it as part of the response. When framework sees the CSRF token
+                 *  in the response header, it takes care of sending the same as Cookie as well. For the same, we need to use CsrfTokenRequestAttributeHandler
+                 *  and create a filter with the name CsrfCookieFilter which runs every time after the Spring Security in built filter BasicAuthenticationFilter
+                 *  like shown below. More details about Filters, are discussed inside the Section 8 of the course.
+                 */
             })
             .and()
-            .csrf()
-                .ignoringRequestMatchers("/register")
-                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .and()
+                .csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact","/register")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+            .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
             .authorizeHttpRequests()
-                .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans", "/contact", "/user").authenticated()
-                .requestMatchers("/notices", "/register").permitAll()
-            .and().formLogin()
-            .and().httpBasic();
-
+            .requestMatchers("/myAccount","/myBalance","/myLoans","/myCards", "/user").authenticated()
+            .requestMatchers("/notices","/contact","/register").permitAll()
+            .and()
+                .formLogin()
+            .and()
+                .httpBasic();
         return http.build();
     }
 
