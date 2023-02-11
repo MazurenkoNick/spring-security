b8e0955217e38155c3caa1c5fd3289c4ee8448ad
nikita <nikitamazurenko42@gmail.com>
2023-02-11 18:14:41 +0200

    assigned to UsernamePasswordAuthenticationToken grantedAuthorities list which is retrieved from the database

--- src/main/java/com/mazurenko/springsecuritybasic/service/UsernamePasswordAuthenticationProvider.java
+++ src/main/java/com/mazurenko/springsecuritybasic/service/UsernamePasswordAuthenticationProvider.java
@@ -1,5 +1,8 @@
 package com.mazurenko.springsecuritybasic.service;
 
+import com.mazurenko.springsecuritybasic.entity.Authority;
+import com.mazurenko.springsecuritybasic.entity.Customer;
+import com.mazurenko.springsecuritybasic.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -7,6 +10,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -14,38 +19,56 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 
 @Component
 public class UsernamePasswordAuthenticationProvider implements AuthenticationProvider {
-    UserDetailsService userDetailsService;
+    CustomerRepository customerRepository;
     PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UsernamePasswordAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
-        this.userDetailsService = userDetailsService;
+    public UsernamePasswordAuthenticationProvider(CustomerRepository customerRepository, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String username = authentication.getName();
-        UserDetails user;
+        Customer user;
 
         // if user doesn't exist - throw an exception
         try {
-            user = userDetailsService.loadUserByUsername(username);
+            user = customerRepository.findByEmail(username);
         }
         catch (UsernameNotFoundException e) {
             throw new BadCredentialsException(e.getMessage());
         }
 
-        // check if passwords match
         String password = authentication.getCredentials().toString();
+        // retrieve authorities of the user (customer) and convert them to GrantedAuthority list
+        List<GrantedAuthority> grantedAuthorities = getGrantedAuthorities(user.getAuthorities());
 
+        // if passwords match - return UsernamePasswordAuthenticationToken;
         if (passwordEncoder.matches(password, user.getPassword())) {
-            return new UsernamePasswordAuthenticationToken(username, password, user.getAuthorities());
+            return new UsernamePasswordAuthenticationToken(username, password, grantedAuthorities);
         }
         else throw new BadCredentialsException("Invalid password!");
+    }
+
+    // method that converts authorities from the database to the GrantedAuthority list
+    private List<GrantedAuthority> getGrantedAuthorities(Collection<? extends Authority> authorities) {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        authorities.stream()
+                .map(Authority::getName)
+                .map(SimpleGrantedAuthority::new)
+                .forEach(grantedAuthorities::add);
+
+        return grantedAuthorities;
     }
 
     @Override
